46c9ccff4bf2c6f2d340b4bd38b4cce84e2af699
Priyanshu8709 <priyanshurajsk_cse23@its.edu.in>
2026-06-01 17:54:02 +0530

    Intial commit

---
+++ src/main/java/com/BookMyStay/bookmystay/BookmystayApplication.java
@@ -0,0 +1,13 @@
+package com.BookMyStay.bookmystay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmystayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmystayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BookMyStay/bookmystay/BookmystayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookMyStay.bookmystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmystayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
